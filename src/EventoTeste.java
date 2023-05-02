b4f5f2ef22a61a6833ce16c33dce62ff7c61c269
matheuspedrosa13 <matheuspmc13@gmail.com>
2023-05-02 16:01:15 -0300

    provocando erro

--- src/EventoTeste.java
+++ src/EventoTeste.java
@@ -1,10 +1,12 @@
 import java.time.LocalDate;
 
 public class EventoTeste {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Evento evento = new Evento("João Silva", "12345678901", "(11) 1234-5678", LocalDate.of(1990, 1, 1), LocalDate.of(2023, 5, 1), Periodo.NOITE);
 
         System.out.println(evento);
+
+        throw new Exception("erro no teste");
     }
 
 }
